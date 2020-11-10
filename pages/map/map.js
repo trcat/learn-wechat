cfe24c9d8269d78edad01674032daa013a73aa79
DayDay <daydaybigking@sina.com>
2020-11-10 16:50:06 +0800

    优化 map 组件的使用
    添加了气泡点等内容

--- pages/map/map.js
+++ pages/map/map.js
@@ -3,9 +3,13 @@
 Page({
   data: {
     longitude: 0,
-    latitude: 0
+    latitude: 0,
+    markers: []
   },
   onLoad() {
+    this.getLocation();
+  },
+  getLocation() {
     wx.getLocation({
       type: 'wgs84',
       success: (res) => {
@@ -13,7 +17,25 @@ Page({
           longitude: res.longitude,
           latitude: res.latitude
         })
+        this.generateMarkers(res.longitude, res.latitude);
       }
     })
+  },
+  generateMarkers(longitude, latitude) {
+    const markers = [];
+    markers.push({
+      id: 1,
+      longitude,
+      latitude,
+      callout: {
+        content: '当前位置',
+        color: '#409EFF',
+        bgColor: 'transparent',
+        display: 'ALWAYS'
+      }
+    });
+    this.setData({
+      markers: markers
+    });
   }
 })
